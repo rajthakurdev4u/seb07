a7203f1b383cfc11209fdd92acf7e1d4b6af17ad
Raj Thakur <raj.thakur@debutinfotech.com>
2022-07-19 13:01:07 +0530

    complete task #9

--- lib/ATESTS/screens/report_user_screen.dart
+++ lib/ATESTS/screens/report_user_screen.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+
+import 'package:aft/ATESTS/models/AUser.dart';
+import 'package:aft/ATESTS/other/AUtils.dart';
+import 'package:aft/ATESTS/provider/AUserProvider.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 class ReportUserScreen extends StatefulWidget {
   const ReportUserScreen({Key? key}) : super(key: key);
@@ -10,8 +17,12 @@ class ReportUserScreen extends StatefulWidget {
 }
 
 class _ReportUserScreenState extends State<ReportUserScreen> {
+  User? user;
+
   @override
   Widget build(BuildContext context) {
+    user = Provider.of<UserProvider>(context).getUser;
+
     return Container(
       color: Colors.white,
       child: SafeArea(
@@ -20,7 +31,14 @@ class _ReportUserScreenState extends State<ReportUserScreen> {
           body: Column(
             children: [
               InkWell(
-                onTap: () {},
+                onTap: () {
+                  // sendEmail(
+                  //   name: user?.username ?? '',
+                  //   email: user?.email ?? '',
+                  //   subject: 'From Admin',
+                  //   message: 'one',
+                  // );
+                },
                 child: Container(
                   color: Colors.blue,
                   width: 150,
@@ -29,7 +47,17 @@ class _ReportUserScreenState extends State<ReportUserScreen> {
                 ),
               ),
               InkWell(
-                onTap: () {},
+                onTap: () {
+                  sendEmail(
+                    receiverName: user?.username ?? '',
+                    // receiverEmail: user?.email ?? '',
+                    receiverEmail: 'rjjin.19@gmail.com',
+                    senderEmail: 'rajthakur.dev4u@gmail.com',
+                    senderName: 'Raj Thakur',
+                    subject: 'From Admin',
+                    message: 'two',
+                  );
+                },
                 child: Container(
                   color: Colors.orange,
                   width: 150,
@@ -42,5 +70,44 @@ class _ReportUserScreenState extends State<ReportUserScreen> {
         ),
       ),
     );
+  }
+
+  Future sendEmail({
+    // Sender
+    required String senderName,
+    required String senderEmail,
+
+    // Receiver
+    required String receiverName,
+    required String receiverEmail,
+
+    // Email
+    required String subject,
+    required String message,
+  }) async {
+    var url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
+    var response = await http.post(
+      url,
+      headers: {
+        'origin': 'http://localhost',
+        'Content-type': 'application/json',
+      },
+      body: jsonEncode({
+        'service_id': 'service_61dg2wq',
+        'template_id': 'template_qsler7e',
+        'user_id': 'mNBhdn2YkFbAHdmzY',
+        'template_params': {
+          'sender_name': senderName,
+          'sender_email': senderEmail,
+          'receiver_name': receiverName,
+          'receiver_email': receiverEmail,
+          'subject': subject,
+          'message': message,
+        }
+      }),
+    );
+
+    print('response: ${response.body}');
+    showSnackBar('Email sent successfully to $receiverEmail.', context);
   }
 }
