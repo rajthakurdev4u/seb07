81f43aa4905fdb89b614b269c903322a1c0956c9
Raj Thakur <raj.thakur@debutinfotech.com>
2022-08-04 17:05:12 +0530

    sync code

---
+++ lib/ATESTS/provider/user_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '../models/user.dart';
+import '../methods/auth_methods.dart';
+
+class UserProvider with ChangeNotifier {
+  User? _user;
+  final AuthMethods _authMethods = AuthMethods();
+
+  User? get getUser => _user;
+
+  Future<void> refreshUser() async {
+    User user = await _authMethods.getUserDetails();
+    _user = user;
+    notifyListeners();
+  }
+
+  Future<void> logoutUser() async {
+    _user = null;
+    notifyListeners();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,66 @@
-import 'package:camera/camera.dart';
+import 'package:aft/ATESTS/provider/user_provider.dart';
+import 'package:aft/ATESTS/responsive/AMobileScreenLayout.dart';
+import 'package:aft/ATESTS/responsive/AResponsiveLayout.dart';
+import 'package:aft/ATESTS/responsive/AWebScreenLayout.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'ATESTS/authentication/ALogin.dart';
-import 'ATESTS/other/AGlobalVariables.dart';
-import 'ATESTS/screens/ACountries.dart';
-import 'ATESTS/provider/AUserProvider.dart';
-import 'ATESTS/responsive/AMobileScreenLayout.dart';
-import 'ATESTS/authentication/ALogin.dart';
-import 'ATESTS/responsive/AMobileScreenLayout.dart';
-import 'ATESTS/responsive/AResponsiveLayout.dart';
-import 'ATESTS/responsive/AWebScreenLayout.dart';
+import 'package:camera/camera.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAi6OvbEXLq9RidJod1bUj9XrPEY2LCBC8",
+        appId: "1:463557381734:web:6b3ea8de7f9376011c27a7",
+        messagingSenderId: "463557381734",
+        projectId: "ft-flutter-15fa1",
+        storageBucket: "ft-flutter-15fa1.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => UserProvider(),
-        ),
-      ],
-      child: MaterialApp(
-        theme: ThemeData(
-          inputDecorationTheme: const InputDecorationTheme(
-            labelStyle: TextStyle(color: Colors.black),
-            hintStyle: TextStyle(color: Colors.grey),
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null)
+          FocusManager.instance.primaryFocus!.unfocus();
+      },
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => UserProvider(),
           ),
-          scaffoldBackgroundColor: const Color.fromARGB(255, 236, 234, 234),
-        ),
-        home: const ResponsiveLayout(
-          mobileScreenLayout: MobileScreenLayout(),
-          webScreenLayout: WebScreenLayout(),
+        ],
+        child: MaterialApp(
+          theme: ThemeData().copyWith(
+            scaffoldBackgroundColor: Colors.white,
+            colorScheme:
+            ThemeData().colorScheme.copyWith(primary: Colors.blueGrey),
+          ),
+          // theme: ThemeData(
+          // inputDecorationTheme: const InputDecorationTheme(
+          //   labelStyle: TextStyle(color: Colors.black),
+          //   hintStyle: TextStyle(color: Colors.grey),
+
+          // scaffoldBackgroundColor: const Color.fromARGB(255, 236, 234, 234),
+
+          title: '',
+          home: const ResponsiveLayout(
+            mobileScreenLayout: MobileScreenLayout(),
+            webScreenLayout: WebScreenLayout(),
+          ),
         ),
       ),
     );
